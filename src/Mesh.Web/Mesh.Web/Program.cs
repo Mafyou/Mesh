2e000d7b1e853c3dab82fe09a7940234d5cec40b
Mafyou <admin@mafyouit.tech>
2026-05-17 12:29:51 +0200

    Web only presenting and flash

--- src/Mesh.Web/Mesh.Web/Program.cs
+++ src/Mesh.Web/Mesh.Web/Program.cs
@@ -4,20 +4,6 @@ builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents()
     .AddInteractiveWebAssemblyComponents();
 
-builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
-    .AddCookie(o =>
-    {
-        o.LoginPath = "/login";
-        o.ExpireTimeSpan = TimeSpan.FromDays(30);
-        o.SlidingExpiration = true;
-    });
-
-builder.Services.AddAuthorization();
-builder.Services.AddCascadingAuthenticationState();
-
-builder.Services.AddSingleton<MeshService>();
-builder.Services.AddSingleton<IMeshService>(sp => sp.GetRequiredService<MeshService>());
-
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
@@ -32,26 +18,7 @@ else
 
 app.UseStatusCodePagesWithReExecute("/not-found", createScopeForStatusCodePages: true);
 app.UseHttpsRedirection();
-app.UseWebSockets();
-app.UseAuthentication();
-app.UseAuthorization();
 app.UseAntiforgery();
-
-// WebSocket endpoint for ESP32 mesh nodes
-// Wire format (first frame): [nodeId 1B][name UTF-8]
-// Subsequent frames: [src 1B][text UTF-8]
-app.Map("/ws/node", async context =>
-{
-    if (!context.WebSockets.IsWebSocketRequest)
-    {
-        context.Response.StatusCode = StatusCodes.Status400BadRequest;
-        return;
-    }
-    var ws = await context.WebSockets.AcceptWebSocketAsync();
-    var svc = context.RequestServices.GetRequiredService<MeshService>();
-    await svc.HandleNodeAsync(ws, context.RequestAborted);
-});
-
 app.MapStaticAssets();
 app.MapRazorComponents<App>()
     .AddInteractiveServerRenderMode()
--- src/Mesh.Web/Mesh.Web/Usings.cs
+++ src/Mesh.Web/Mesh.Web/Usings.cs
@@ -1,8 +1,2 @@
-﻿global using Mesh.Web.Client.Services;
 global using Mesh.Web.Components;
-global using Mesh.Web.Services;
-global using Microsoft.AspNetCore.Authentication.Cookies;
-global using System.Collections.Concurrent;
-global using System.Net.WebSockets;
 global using System.Text;
-global using System.Text.Json;
